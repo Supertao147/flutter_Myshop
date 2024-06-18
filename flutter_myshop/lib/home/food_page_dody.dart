81cf269b3113830185ef70c9c47d19cdfc2f26e4
Pawarit Poolso <supertao147@gmail.com>
2024-06-18 14:55:11 +0700

    Update food_page_dody.dart

--- flutter_myshop/lib/home/food_page_dody.dart
+++ flutter_myshop/lib/home/food_page_dody.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_myshop/utils/colors.dart';
+import 'package:flutter_myshop/widgets/big_text.dart';
+import 'package:flutter_myshop/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -8,24 +13,73 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
+      height: 320,
       child: PageView.builder(
-        itemCount: 5,
-        itemBuilder: (context ,position){
-        return _buildPageItem(position);
-      }),
+          controller: pageController,
+          itemCount: 5,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
     );
   }
-  Widget _buildPageItem(int index){
-  return Container(
-      height: 220,
-      margin:  EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius:  BorderRadius.circular(30),
-        color: Color(0xFF69c5df)
-      ),
+
+  Widget _buildPageItem(int index) {
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/food0.png"))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "ป้าไร อาหารตามสั่ง"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) {return Icon(
+                            Icons.star,
+                            color: AppColors.maimColor,
+                            size: 15,
+                          );}),
+                      ),
+                      SizedBox(width: 10,),
+                      SmallText(text: "4.5")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
